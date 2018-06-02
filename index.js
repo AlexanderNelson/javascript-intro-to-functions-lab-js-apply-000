060d2658ec3eb2e2cf093452ed41ea01dbd1e939
Alexander Nelson <amvetserv@gmail.com>
2018-06-02 16:54:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,4 +17,6 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
  string.toLowerCase() === string
  return "I can't hear you!"
+ string.toUpperCase() === string
+ return "YES INDEED!"
 }
